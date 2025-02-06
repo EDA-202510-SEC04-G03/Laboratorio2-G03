4de73cebf1e269be0c7aee74d785ea1701d5a037
JJAS <juanjamador2006@gmail.com>
2025-02-05 19:35:45 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -72,7 +72,7 @@ def load_books(app):
     :type app: logic
     """
     books = logic.load_books(app,
-                             "GoodReads/books-small.csv")
+                             "GoodReads/books.csv")
     return books
 
 
@@ -98,7 +98,7 @@ def load_books_tags(app):
     :type app: logic
     """
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    books_tags = logic.load_books_tags(app,"GoodReads/book_tags-small.csv")
+    books_tags = logic.load_books_tags(app,"GoodReads/book_tags.csv")
     return books_tags
    
 def first_book(app):
